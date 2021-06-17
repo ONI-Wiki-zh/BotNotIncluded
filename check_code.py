a296c65136881ae99178c206e2f22ffa8c191c3a
DDElephant <andy-ding@outlook.com>
2021-06-17 01:43:02 -0700

    Update check_code.py

--- check_code.py
+++ check_code.py
@@ -2,6 +2,7 @@ import utils
 import os
 import os.path as path
 
+target = input("Enter string to search in code")
 for root, subdirs, files in os.walk(utils.DIR_CODE):
     for file in files:
         if not file.endswith(".cs"):
@@ -10,5 +11,5 @@ for root, subdirs, files in os.walk(utils.DIR_CODE):
         f_path = path.join(root, file)
         with open(f_path, 'rb') as f:
             f_content = f.read().decode("utf-8")
-            if input("Enter string to search in code") in f_content:
+            if target in f_content:
                 print(f_path)
