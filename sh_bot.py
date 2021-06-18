f2822045317015990da4f7fe0eb7fb21c53ffbfe
DDElephant <andy-ding@outlook.com>
2021-06-17 23:17:57 -0700

    doc

--- sh_bot.py
+++ sh_bot.py
@@ -2,11 +2,11 @@ import json
 import logging
 import os.path as path
 import pathlib
+import sys
 from typing import Union
 
 import pywikibot
 import pywikibot.textlib as textlib
-import sys
 
 DIR_TMP = "tmp"
 PATH_CONFIG = path.join(DIR_TMP, "config.json")
@@ -30,11 +30,20 @@ class Config:
 
     @staticmethod
     def bold_head(s: str):
+        """ Format a string so it is shown in bold and as head in command line / stdout. """
         return f"{Config.CODE_HEAD}{Config.CODE_BOLD}{s}{Config.CODE_END}{Config.CODE_END}"
 
-    def __init__(self, test: bool = False, mute: bool = False, auto: bool = False, clear_old: bool = False):
+    def __init__(self, test: bool = False, mute: bool = None, auto: bool = False, clear_old: bool = False):
+        """ Script config class, 2 methods of which need to be implemented by overriding.
+
+        :param test: Enable test mode and avoid written to wiki site. Note that login is not required in this mode.
+        :param mute: Block some verbose output, including the the page content to save. Default to False on test mode.
+        :param auto: Apply auto_rule with out asking with input box
+        :param clear_old: Clear old history containing rules defined before.
+        A double confirmation mechanism is implemented.
+        """
         self.test = test
-        self.mute = mute
+        self.mute = (not test) if mute is None else mute
         self.auto = auto
         if clear_old:
             ans = input("Are you sure you want to clear config history? Type yes to continue.\n")
@@ -64,17 +73,27 @@ class Config:
         raise NotImplementedError()
 
     def _app_default_rule(self, s: str):
+        """ Wrapper of self.default_rule. """
         out = self.default_rule(s)
         if out is not None:
             logger.info(f"Defuault rule applied to {s}, got {repr(out)}")
         return out
 
     def _app_auto_rule(self, s: str):
+        """ Wrapper of self.auto_rule. """
         out = self.auto_rule(s)
         logger.info(f"Auto rule applied to '{s}', got {repr(out)}")
         return out
 
     def cat_map(self, cat: pywikibot.Category, is_re=False) -> Union[bool, str]:
+        """ Transfer (maybe with the help of user input) category on source site to that used in target site.
+
+        :param cat: The category on source site
+        :param is_re: If this method is called when encountering a
+        redirection. A warning will be generated based on this parameter
+        :return: The category name used in target site, or False, indicating
+        that this category is discarded when transferring files.
+        """
         cat_name = cat.title(with_ns=False)
         if cat_name not in self.config["cate_map"]:
             name = self._app_default_rule(cat_name)
@@ -101,11 +120,20 @@ class Config:
             fh.write(s.encode("utf-8"))
 
 
-def sync_cate(source: pywikibot.Page, target: pywikibot.Page, config: Config, is_re=False, ):
+def sync_cate(source: pywikibot.Page, target: pywikibot.Page, conf: Config, is_re=False):
+    """ Add categories of source page to target page according to the matching rule defined in config.
+
+
+    :param source: Source page
+    :param target: Target page
+    :param conf: Config object
+    :param is_re: If these two pages are redirect pages.
+    :return:
+    """
     new_cats = []
     old_cats = list(target.categories())
     for c_source in source.categories():
-        c_target_name = config.cat_map(c_source, is_re=is_re)
+        c_target_name = conf.cat_map(c_source, is_re=is_re)
         if not c_target_name:
             continue
         new_cat = pywikibot.Category(target.site, c_target_name)
@@ -118,12 +146,14 @@ def sync_cate(source: pywikibot.Page, target: pywikibot.Page, config: Config, is
 
 
 def getFinalRedirectTarget(page: pywikibot.Page):
+    """ Continuously get redirect target until a non-redirect page encountered. """
     while page.isRedirectPage():
         page = page.getRedirectTarget()
     return page
 
 
 def get_files(target: pywikibot.Site, summary: dict):
+    """ Get all file used in a site. """
     scanned_files = set()
     all_pages = list(target.allpages())
     for i, p in enumerate(all_pages):
@@ -136,10 +166,16 @@ def get_files(target: pywikibot.Site, summary: dict):
     return scanned_files
 
 
-def save_page(page: pywikibot.Page, config: Config, summary):
-    if not config.test:
+def save_page(page: pywikibot.Page, conf: Config, summary):
+    """ Page saving behavior under both normal and test mode. See **pywikibot.Page.save** for more details.
+
+    :param page: The page to save
+    :param conf: Config object
+    :param summary: Summary object which records some statistics while running
+    """
+    if not conf.test:
         page.save(summary=EDIT_SUMMARY)
-    elif not config.mute:
+    elif not conf.mute:
         width = 80
         page_width = len(page.title()) + 2
         l_half = (width - page_width) // 2
@@ -153,16 +189,35 @@ def save_page(page: pywikibot.Page, config: Config, summary):
     summary["page_saved"] += 1
 
 
-def upload_file(page: pywikibot.FilePage, source: str, config: Config, summary, text=None, report_success=None):
-    if not config.test:
+def upload_file(page: pywikibot.FilePage, source: str, conf: Config, summary, text=None, report_success=None):
+    """ File uploading behavior under both normal and test mode. See **pywikibot.page.FilePage** for more details.
+
+    :param page: File page to upload to
+    :param source: path or url of the image to be uploaded
+    :param conf: Config object
+    :param summary: Summary object which records some statistics while running
+    :param text: Initial page text
+    :param report_success: If to report success uploading.
+    """
+    if not conf.test:
         page.upload(source, text=text, report_success=report_success)
-    elif not config.mute:
+    elif not conf.mute:
         logger.info(f"[TEST MODE]: UPLOAD file to page '{page.title()}' with '{source}'")
     summary["uploaded"] += 1
 
 
-def sync_files(source: pywikibot.Site, target: pywikibot.Site, scanned_files: set, summary: dict, config: Config):
+def sync_files(source: pywikibot.Site, target: pywikibot.Site, scanned_files: set, summary: dict, conf: Config):
+    """ Sync a set of files (together files which share categories with them on the source site) on target
+    site with that on source site.
+
+    :param source: Source site
+    :param target: Target site
+    :param scanned_files: File list to sync
+    :param summary: Summary object which records some statistics while running
+    :param conf: Config object
+    """
     viewed_source_cat = set()
+    synced_file_page = set()
     for i, f in enumerate(scanned_files):
         f_target = pywikibot.FilePage(target, f)
         if not f.endswith(".svg"):
@@ -185,8 +240,8 @@ def sync_files(source: pywikibot.Site, target: pywikibot.Site, scanned_files: se
 
             f_target.set_redirect_target(
                 pywikibot.Page(target, f_source.getRedirectTarget().title(with_ns=True)), save=False, force=True)
-            sync_cate(f_source, f_target, config, is_re=True)
-            save_page(f_target, config, summary)
+            sync_cate(f_source, f_target, conf, is_re=True)
+            save_page(f_target, conf, summary)
             f_target = f_target.getRedirectTarget()
             f_source = f_source.getRedirectTarget()
             redirected = True
@@ -197,7 +252,7 @@ def sync_files(source: pywikibot.Site, target: pywikibot.Site, scanned_files: se
         for cat_source in f_source.categories():
             if cat_source in viewed_source_cat:
                 continue
-            cat_target_name = config.cat_map(cat_source)
+            cat_target_name = conf.cat_map(cat_source)
             if not cat_target_name:
                 logger.info(f"Skipped {cat_source.title(with_ns=False)} when checking {f_target}")
                 viewed_source_cat.add(cat_source)
@@ -205,14 +260,17 @@ def sync_files(source: pywikibot.Site, target: pywikibot.Site, scanned_files: se
 
             for sibling_source in cat_source.members(namespaces="File"):
                 sibling_target = pywikibot.FilePage(target, sibling_source.title(with_ns=False))
+                if sibling_target in synced_file_page:
+                    continue
                 if not sibling_target.exists():
-                    upload_file(sibling_target, sibling_source.get_file_url(), config, summary, text="",
+                    upload_file(sibling_target, sibling_source.get_file_url(), conf, summary, text="",
                                 report_success=True)
-                    sync_cate(sibling_source, sibling_target, config)
-                    save_page(sibling_target, config, summary)
+                    sync_cate(sibling_source, sibling_target, conf)
+                    save_page(sibling_target, conf, summary)
                 else:
-                    sync_cate(sibling_source, sibling_target, config)
-                    save_page(sibling_target, config, summary)
+                    sync_cate(sibling_source, sibling_target, conf)
+                    save_page(sibling_target, conf, summary)
+                synced_file_page.add(sibling_target)
             viewed_source_cat.add(cat_source)
         logger.info(f"\033[92mFile processed: {i + 1}/{len(scanned_files)}\033[0m\n\n")
 
