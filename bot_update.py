a8722cf78d746422f9c64458261b57d77c7806a2
DDElephant <andy-ding@outlook.com>
2021-07-28 20:58:09 -0700

    Create bot_update.py

---
+++ bot_update.py
@@ -0,0 +1,36 @@
+import pywikibot
+
+
+def bot_update(site: pywikibot.Site, source: pywikibot.Site):
+    to_update = {
+        "outdated": [],
+        "non-existence": [],
+    }
+    all_pages = list(site.allpages(content=True))
+    for i, p in enumerate(all_pages):
+        p: pywikibot.Page
+        if i % 10 == 0:
+            print(f"{i}/{len(all_pages)}")
+        source_links = [link for link in p.langlinks() if link.site == source]
+        if len(source_links) == 0:
+            continue
+        source_page = pywikibot.Page(source_links[0])
+        if not source_page.exists():
+            to_update["non-existence"].append(p)
+        elif p.latest_revision.timestamp < source_page.latest_revision.timestamp:
+            to_update["outdated"].append(p)
+
+    log_page = pywikibot.Page(site, "project:Log/Sync EN")
+    log_page.text = f"本页面更新于 UTC {pywikibot.datetime.datetime.utcnow()}，" \
+                    f"代码详见 [https://github.com/DDEle/BotNotIncluded GitHub 仓库]。\n\n"
+    for cat in to_update:
+        log_page.text += cat + "\n"
+        log_page.text += "".join(f"* [[{p.title()}]]\n" for p in to_update[cat])
+    log_page.save("bot_update: Outdated pages compared with EN site")
+    return to_update
+
+
+if __name__ == '__main__':
+    oni_zh = pywikibot.Site("zh", "oni")
+    oni_en = pywikibot.Site("en", "oni")
+    outdated = bot_update(oni_zh, oni_en)
