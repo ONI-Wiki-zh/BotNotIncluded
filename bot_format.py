ab395ade12b7758f6ac786f578d0fcf8e6716c39
DDElephant <andy-ding@outlook.com>
2022-01-27 21:56:07 -0800

    老旧的X光照片

--- bot_format.py
+++ bot_format.py
@@ -65,6 +65,7 @@ def format_str(text: str):
 
     # special case due to official translation
     text = re.sub(rf'[实试]验体 ?52 ?B', r"试验体52B", text)
+    text = re.sub(rf'老旧的 ?X ?光照片', r"老旧的X光照片", text)
     text = re.sub(rf'低温箱 ?3000', r"低温箱3000", text)
     text = re.sub(rf'炊 ?CC', r"炊CC", text)
     text = re.sub(rf'- ?啪叽 ?-', r"-啪叽-", text)
