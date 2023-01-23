11f14713ec260d7a56dc47d0426df8cd52ab9f50
DDElephant <andy-ding@outlook.com>
2023-01-23 21:08:47 +0800

    except unknown interwiki site

--- bot_update.py
+++ bot_update.py
@@ -26,28 +26,32 @@ def bot_update(site: pywikibot.Site, target: pywikibot.Site):
         p: pywikibot.Page
         if i % 10 == 0:
             logger.info(f"Page inter-lang checked: {i}/{len(all_pages)}")
-        source_links = [link for link in p.langlinks() if link.site == target]
-        if len(source_links) == 0:
-            continue
-        target_page = pywikibot.Page(source_links[0])
-        if not target_page.exists():
-            to_update["non-existence"].append(p)
-        else:
-            back = [link for link in target_page.langlinks() if link.site == site]
-            if len(back) == 0:
-                to_update["oneway"].append(p)
+        try:
+            source_links = [link for link in p.langlinks() if link.site == target]
+            if len(source_links) == 0:
                 continue
-            elif back[0].title != p.title():
-                to_update['non-unique'].append(p)
+            target_page = pywikibot.Page(source_links[0])
+            if not target_page.exists():
+                to_update["non-existence"].append(p)
             else:
-                for r in target_page.revisions():
-                    if r.user in zh_contributors:
-                        continue
-                    if "zh link".upper() in r.comment.upper():
-                        continue
-                    if r.timestamp > p.latest_revision.timestamp:
-                        to_update["outdated"].append(p)
-                    break
+                back = [link for link in target_page.langlinks() if link.site == site]
+                if len(back) == 0:
+                    to_update["oneway"].append(p)
+                    continue
+                elif back[0].title != p.title():
+                    to_update['non-unique'].append(p)
+                else:
+                    for r in target_page.revisions():
+                        if r.user in zh_contributors:
+                            continue
+                        if "zh link".upper() in r.comment.upper():
+                            continue
+                        if r.timestamp > p.latest_revision.timestamp:
+                            to_update["outdated"].append(p)
+                        break
+        except pywikibot.exceptions.UnknownSiteError as e:
+            logger.warn(f"UnknownSiteError when checking {p.title()}: {str(e)}")
+            continue
 
     log_page = pywikibot.Page(site, "project:Log/Sync EN")
     log_page.text = "本页面更新于 {{REVISIONTIMESTAMP}}，" \
