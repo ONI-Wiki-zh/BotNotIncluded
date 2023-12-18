ee8b7d9b7713c5d9f3396d6e2256650d1206376c
tuodekun <47078367+tuodekun@users.noreply.github.com>
2023-12-18 22:49:03 +0800

    new login method for pywikibot 8.6.0

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def login(site: pywikibot.APISite):
     retry = 0
     while not site.logged_in() and retry < 5:
         retry += 1
-        login_manager = pywikibot.data.api.LoginManager(
+        login_manager = pywikibot.login.LoginManager(
             site=site,
             user=f'{os.environ.get("BOT_NAME")}@GithubActions',
             password=os.environ.get("BOT_PASS")
