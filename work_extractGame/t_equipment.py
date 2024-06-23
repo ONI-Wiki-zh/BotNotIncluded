e846730bbc5f85c89b30481447503bf033090b8f
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-06-23 18:16:09 +0800

    update equipment schema

--- work_extractGame/t_equipment.py
+++ work_extractGame/t_equipment.py
@@ -11,15 +11,29 @@ def createWornItem(wornId, item, equipDef):
     """损坏装备"""
     item_worn = item.copy()
     # 损坏物品
-    item_worn['name'] = wornId
+    item_worn['isWorn'] = True
+    item_worn['id'] = wornId
     item_worn['nameString'] = equipDef.get('WornName', None)
+    if item_worn.get('attribute', None):
+        del item_worn['attribute']
+    if item_worn.get('suitTank', None):
+        del item_worn['suitTank']
+    if item_worn.get('onEquipEffect', None):
+        del item_worn['onEquipEffect']
+    if item_worn.get('leadSuitTank', None):
+        del item_worn['leadSuitTank']
+    if item_worn.get('effectImmunites', None):
+        del item_worn['effectImmunites']
+    # 标签
     AdditionalTags = equipDef.get('AdditionalTags', [])
     if AdditionalTags:
         list_add_tags = [mItem['Name'] for mItem in AdditionalTags]
-        if item_worn.get('tags', None):
-            item_worn['tags'] = item_worn['tags'].extend(list_add_tags)
-        else:
-            item_worn['tags'] = list_add_tags
+        if list_add_tags and len(list_add_tags) > 0:
+            list_tags = item_worn.get('tags', None)
+            if list_tags and len(list_tags) > 0:
+                item_worn['tags'] = list(set(list_tags + list_add_tags))
+            else:
+                item_worn['tags'] = list_add_tags
     return item_worn
 
 
@@ -36,14 +50,17 @@ def convert_data_2_lua(entityInfo: EntityInfo):
             entityId = item.get('name', None)
             if entityId is None:
                 continue
+            item['id'] = entityId
             if item.get('tags', None):
                 item['tags'] = [mItem['Name'] for mItem in item['tags']]
             equipDef = dict_equipmentDefs[entityId]
             if equipDef:
                 item['slot'] = equipDef['Slot']
+                # 定义损坏物品
                 wornId = equipDef.get('wornID', None)
                 if wornId:
                     dict_output[wornId] = createWornItem(wornId, item, equipDef)
+                item['wornId'] = wornId
             # 损坏物品,不存在以下属性
             if equipDef:
                 # 防护属性
