0a878c2355d6cb78549aa4c4c8bd71a379a35f97
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-06-26 23:38:33 +0800

    update building to lua convert

--- work_extractGame/t_building.py
+++ work_extractGame/t_building.py
@@ -2,7 +2,11 @@ import json
 
 import work_extractGame.constant_extract as constant
 from work_extractGame.model.EntityInfo import EntityInfo
-from work_extractGame.util.DataUtils import save_lua_by_schema
+from work_extractGame.util.DataUtils import save_lua_by_schema, DataUtils, getPOEntry_by_nameString
+
+dict_SimHashes = None
+dict_Diseases = None
+dict_grantSkill = None
 
 
 def getRoomRequireTags(entity, roomConstraintTags):
@@ -32,6 +36,7 @@ def getIngredients(buildDef):
 
 
 def getRocketModule(entity):
+    """获取火箭模块信息"""
     if entity is None:
         return None
     rocketModule = entity.get('rocketModule', None)
@@ -41,6 +46,7 @@ def getRocketModule(entity):
 
 
 def getStorageInfo(entity):
+    """获取内部存储信息"""
     if entity is None:
         return None
     storage = entity.get('storage', None)
@@ -57,18 +63,33 @@ def getStorageInfo(entity):
 
 
 def getRocketEngineCluster(entity):
+    """获取火箭引擎信息"""
     if entity is None:
         return None
+    global dict_SimHashes
+    if dict_SimHashes is None:
+        dict_SimHashes = DataUtils.loadSimHashed()
+    global dict_Diseases
+    if dict_Diseases is None:
+        dict_Diseases = DataUtils.loadSimHashed_disease()
     rocketEngineCluster = entity.get('rocketEngineCluster', None)
     if rocketEngineCluster:
         fuelTag = rocketEngineCluster.get('fuelTag')
         if fuelTag:
             rocketEngineCluster['fuelTag'] = fuelTag['Name']
+        exhaustElement = rocketEngineCluster.get('exhaustElement', None)
+        if exhaustElement:
+            rocketEngineCluster['exhaustElement'] = dict_SimHashes.get(exhaustElement, None)
+        exhaustDiseaseIdx = rocketEngineCluster.get('exhaustDiseaseIdx', None)
+        if exhaustDiseaseIdx:
+            diseasId = dict_Diseases.get(exhaustDiseaseIdx, None)
+            rocketEngineCluster['exhaustDiseaseIdx'] = diseasId
         return rocketEngineCluster
     return None
 
 
 def getEntityTags(entity):
+    """获取建筑标签tags"""
     if entity is None:
         return None
     tags = entity.get('tags', None)
@@ -78,6 +99,7 @@ def getEntityTags(entity):
 
 
 def getCategory(entity, dict_category):
+    """获取建筑分类"""
     if entity is None:
         return None, None
     category, sub_category = dict_category.get(entity['name'], (None, None))
@@ -91,6 +113,36 @@ def getCategory(entity, dict_category):
     return category, sub_category
 
 
+def getRequireGrantSkill(grantSkillId):
+    """获取复制人操作建筑所需技能"""
+    if grantSkillId is None:
+        return None
+    global dict_grantSkill
+    if dict_grantSkill is None:
+        dict_grantSkill = {}
+        with open(constant.dict_PATH_EXTRACT_FILE['codex'], 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for key, codexEntry in data['categoryEntries'].items():
+                codexId = codexEntry.get('id', None),
+                if codexId:
+                    name = codexEntry.get('name', None)
+                    poEntry, _ = getPOEntry_by_nameString(name, msg_need=['DUPLICANTS.ROLES.', "DUPLICANTS.TRAITS."])
+                    if poEntry:
+                        dict_grantSkill[key] = {
+                            "id": key,
+                            "name": name,
+                            "msgctxt": poEntry.msgctxt,
+                            "parentId": codexEntry.get('parentId', None),
+                            "dlcIds": codexEntry.get('dlcIds', None)
+                        }
+    grankSkill = dict_grantSkill.get(str(grantSkillId).upper(), None)
+    if grankSkill:
+        return grankSkill.get('msgctxt', None)
+    else:
+        print("No Match GrantSkill: "+grantSkillId)
+    return None
+
+
 def convert_data_2_lua(entityInfo: EntityInfo):
     dict_entity = {}
     dict_requiredSkillPerk = {}
@@ -100,25 +152,32 @@ def convert_data_2_lua(entityInfo: EntityInfo):
     # 读取数据
     with open(constant.dict_PATH_EXTRACT_FILE['building'], 'r', encoding='utf-8') as file:
         data = json.load(file)
+        # 房间建筑类型标签
         for item in data.get('roomConstraintTags', None):
             if item.get('IsValid', None):
                 roomConstraintTags.append(item['Name'])
+        # 操作需求技能
         for id, skillPerkId in data.get('requiredSkillPerkMap', None).items():
             dict_requiredSkillPerk[id] = skillPerkId
+        # 建筑分类
         for category, cateList in data.get('buildingAndSubcategoryDataPairs', None).items():
             for cateInfo in cateList:
                 dict_category[cateInfo['Key']] = (category, cateInfo['Value'])
+        # 建筑实体信息
         for item in data.get('bBuildingDefList', None):
             dict_entity[item['name']] = item
+        # 建筑信息
         data_buildDef.extend(data.get("buildingDefs", None))
     with open(constant.dict_PATH_EXTRACT_FILE_BASE_ONLY['building'], 'r', encoding='utf-8') as file:
         data = json.load(file)
+        # 建筑实体信息
         for item in data.get('bBuildingDefList', None):
             id = item['name']
             if id not in dict_entity.keys():
                 print("base only: " + id)
                 dict_entity[id] = item
-        for item in data.get('bBuildingDefList', None):
+        # 建筑信息
+        for item in data.get('buildingDefs', None):
             id = item['name']
             if id not in dict_entity.keys():
                 data_buildDef.append(item)
@@ -136,6 +195,8 @@ def convert_data_2_lua(entityInfo: EntityInfo):
             for unlockedItem in unlockedItems:
                 dict_building_tech[unlockedItem['Id']] = unlockedItem.get('parentTechId')
         for skill in data['skills']:
+            if skill.get('deprecated', True):
+                continue
             perks = skill.get('perks', None)
             if perks is None:
                 continue
@@ -156,7 +217,7 @@ def convert_data_2_lua(entityInfo: EntityInfo):
         ReplacementTags = item.get('ReplacementTags', None)
         if ReplacementTags:
             item['ReplacementTags'] = [tag['Name'] for tag in ReplacementTags]
-        item['requiredGrantSkill'] = dict_perk_skill.get(dict_requiredSkillPerk.get(id, None), None)
+        item['requiredGrantSkill'] = getRequireGrantSkill(dict_perk_skill.get(dict_requiredSkillPerk.get(id, None), None))
         category, sub_category = getCategory(item, dict_category)
         item['category'] = category
         item['subCategory'] = sub_category
