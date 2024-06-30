dfaa99fae6747513d0cb5cb670d404fb45528180
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-06-30 18:12:17 +0800

    bugfix 技术缺少本体内容

--- work_extractGame/t_tech.py
+++ work_extractGame/t_tech.py
@@ -14,26 +14,33 @@ def getContainDlcs(unlockedItems):
     return list(set(list_dlcs))
 
 
+def setTech(item, dic_output):
+    id = item.get('Id', None)
+    requiredTechIDs = item.get('requiredTechIDs', None)
+    if requiredTechIDs is not None and len(requiredTechIDs) <= 0:
+        item['requiredTechIDs'] = None
+    unlockedTechIDs = item.get('unlockedTechIDs', None)
+    if unlockedTechIDs is not None and len(unlockedTechIDs) <= 0:
+        item['unlockedTechIDs'] = None
+    unlockedItems = item.get('unlockedItems', None)
+    if unlockedItems:
+        item['dlcIds'] = getContainDlcs(unlockedItems)
+    if id not in dic_output.keys():
+        dic_output[id] = item
+
+
 def convert_data_2_lua(entityInfo: EntityInfo):
+    dict_output = {}
     # 读取数据
     with open(constant.dict_PATH_EXTRACT_FILE['db'], 'r', encoding='utf-8') as file:
         data = json.load(file).get("techs", None)
-    if data is None:
-        return False
+        for item in data:
+            setTech(item, dict_output)
+    with open(constant.dict_PATH_EXTRACT_FILE_BASE_ONLY['db'], 'r', encoding='utf-8') as file:
+        data = json.load(file).get("techs", None)
+        for item in data:
+            setTech(item, dict_output)
     # id筛选
-    dict_output = {}
-    for item in data:
-        id = item.get('Id', None)
-        requiredTechIDs = item.get('requiredTechIDs', None)
-        if requiredTechIDs is not None and len(requiredTechIDs) <= 0:
-            item['requiredTechIDs'] = None
-        unlockedTechIDs = item.get('unlockedTechIDs', None)
-        if unlockedTechIDs is not None and len(unlockedTechIDs) <= 0:
-            item['unlockedTechIDs'] = None
-        unlockedItems = item.get('unlockedItems', None)
-        if unlockedItems:
-            item['dlcIds'] = getContainDlcs(unlockedItems)
-        dict_output[id] = item
     save_lua_by_schema(entityInfo, dict_output)
     return True
 
