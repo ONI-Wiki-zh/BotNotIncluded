d81c839976d0969597278ac902ce0548312470e7
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-07-16 23:05:51 +0800

    update oni_family

--- site_families/oni_family.py
+++ site_families/oni_family.py
@@ -4,12 +4,12 @@ from pywikibot import family
 class Family(family.Family):
     name = 'oni'
     langs = {
-        'en': 'oxygennotincluded.fandom.com',
-        'zh': 'oxygennotincluded.fandom.com',
-        'ja': 'oxygennotincluded.fandom.com',
-        'pt-br': 'oxygennotincluded.fandom.com',
-        'ru': 'oxygen-not-included.fandom.com',
-        'th': 'oxygennotincluded.fandom.com',
+        'en': 'oxygennotincluded.wiki.gg',
+        'zh': 'oxygennotincluded.wiki.gg',
+        'ja': 'oxygennotincluded.wiki.gg',
+        'pt-br': 'oxygennotincluded.wiki.gg',
+        'ru': 'oxygen-not-included.wiki.gg',
+        'th': 'oxygennotincluded.wiki.gg',
     }
 
     def scriptpath(self, code):
