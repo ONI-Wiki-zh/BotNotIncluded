2691bab3030afa5e662c82ad05a574250faaee3d
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-08-21 02:03:11 +0800

    update MeteorShower export

--- work_extractGame/t_meteorShower.py
+++ work_extractGame/t_meteorShower.py
@@ -2,11 +2,12 @@ import json
 
 import constant as constant
 from work_extractGame.model.EntityInfo import EntityInfo
-from work_extractGame.util.DataUtils import save_lua_by_schema, DataUtils
+from work_extractGame.util.DataUtils import save_lua_by_schema, DataUtils, getPOEntry_by_nameString
 
 
 def convert_data_2_lua(entityInfo: EntityInfo):
     dict_seasons = {}
+    dict_clusterMapMeteorShowerName = {}
     # 读取数据
     with open(constant.dict_PATH_EXTRACT_FILE['db'], 'r', encoding='utf-8') as file:
         gameplaySeasons = json.load(file).get("gameplaySeasons", None)
@@ -19,7 +20,16 @@ def convert_data_2_lua(entityInfo: EntityInfo):
                     dict_seasons[eventId].append(season['Id'])
 
     with open(constant.dict_PATH_EXTRACT_FILE['multiEntities'], 'r', encoding='utf-8') as file:
-        data = json.load(file).get("meteorShowerEventMap", None)
+        fileData = json.load(file)
+        multiEntities = fileData.get('multiEntities', None)
+        for multiEntity in multiEntities:
+            clusterMapMeteorShowerDef = multiEntity.get('clusterMapMeteorShowerDef', None)
+            entityType = multiEntity.get('entityType', None)
+            if entityType == "ClusterMapMeteorShowerConfig" and clusterMapMeteorShowerDef:
+                poEntry, _ = getPOEntry_by_nameString(clusterMapMeteorShowerDef['name'], msg_need=['UI.SPACEDESTINATIONS.CLUSTERMAPMETEORSHOWERS.'])
+                if poEntry:
+                    dict_clusterMapMeteorShowerName[clusterMapMeteorShowerDef['eventID']] = poEntry.msgctxt
+        data = fileData.get("meteorShowerEventMap", None)
         if data is None:
             return False
     # id筛选
@@ -29,6 +39,9 @@ def convert_data_2_lua(entityInfo: EntityInfo):
             tags = [tag['Name'] for tag in item['tags']]
             item['tags'] = tags
         item['seasons'] = dict_seasons.get(eventId, None)
+        clusterMapName = dict_clusterMapMeteorShowerName.get(eventId, None)
+        if clusterMapName:
+            item['clusterMapName'] = clusterMapName
         dict_output[eventId] = item
     save_lua_by_schema(entityInfo, dict_output)
     return True
